0f2ead01e792e7ca93dae5d7a5369b18f94fc806
sammimagg <39244791+sammimagg@users.noreply.github.com>
2022-09-02 13:07:13 +0000

    Version 0.01
    Basic config

---
+++ Group8/src/main/java/is/hi/hbv501g/group8/Group8Application.java
@@ -0,0 +1,14 @@
+package is.hi.hbv501g.group8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+public class Group8Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Group8Application.class, args);
+    }
+
+}
---
+++ Group8/src/test/java/is/hi/hbv501g/group8/Group8ApplicationTests.java
@@ -0,0 +1,13 @@
+package is.hi.hbv501g.group8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Group8ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
