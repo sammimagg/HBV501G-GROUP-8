0ee74b6918f7b164bb1c62c01fd88cd9b1e651de
kbg15 <kbg15@hi.is>
2022-09-16 12:57:05 +0000

    database Schema

---
+++ Group8/SQL/schema.sql
@@ -0,0 +1,34 @@
+CREATE TABLE IF NOT EXISTS Employees(
+    SSN varchar(10) primary key,
+    firstName varchar(128),
+    lastName varchar(128) not null,
+    company varchar(32) not null,
+    jobTitle varchar(128) not null,
+    salary double,
+    phoneNumber varchar(128) not null,
+);
+
+CREATE TABLE IF NOT EXISTS Overview(
+    SSN varchar(10) primary key,
+    clockIn TIMESTAMP NOT NULL,
+    clockOut TIMESTAMP,
+    finished BOOLEAN
+);
+
+CREATE TABLE IF NOT EXISTS Companies (
+    SSN varchar(10) primary key,
+    companyName varchar(32) NOT NULL,
+    companyAddress varchar(128) NOT NULL,
+    companyManager varchar(128) NOT NULL,
+    phoneNumber varchar(128)
+);
+
+CREATE TABLE IF NOT EXISTS Users (
+    SSN varchar(10) PRIMARY KEY,
+    accountType INTEGER,
+    userName varchar(255) NOT NULL,
+    password varchar(255) NOT NULL,
+    constraint valid_type check (accountType < 9)
+);
+
+/*INSERT INTO users (username, password) VALUES ('admin', '$2a$11$pgj3.zySyFOvIQEpD7W6Aund1Tw.BFarXxgLJxLbrzIv/4Nteisii');*/
