399918689dbf6649ad9ff8b1b954c11576494e7d
dorijv <hjv6@hi.is>
2022-09-22 16:53:43 +0000

    Smá cleanup

--- src/main/java/is/hi/hbv501g/group8/Group8Application.java
+++ src/main/java/is/hi/hbv501g/group8/Group8Application.java
@@ -6,7 +6,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.PreparedStatementCallback;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
