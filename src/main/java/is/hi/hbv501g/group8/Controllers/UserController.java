06c34961252ffa877964478af82518754bb00fd9
sammimagg <sammimagg@gmail.com>
2022-10-22 02:16:31 +0000

    Template for missing pages

--- src/main/java/is/hi/hbv501g/group8/Controllers/UserController.java
+++ src/main/java/is/hi/hbv501g/group8/Controllers/UserController.java
@@ -11,6 +11,7 @@ package is.hi.hbv501g.group8.Controllers;
 
 import is.hi.hbv501g.group8.Persistence.Entities.DateHelper;
 import is.hi.hbv501g.group8.Persistence.Entities.User;
+import is.hi.hbv501g.group8.Services.EmployeeService;
 import is.hi.hbv501g.group8.Services.UserService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,6 +26,7 @@ import javax.servlet.http.HttpSession;
 @Controller
 public class UserController {
     UserService userService;
+    EmployeeService employeeService;
 
     /**
      * Vantar lýsingu..
@@ -33,8 +35,9 @@ public class UserController {
      */
 
     @Autowired
-    public UserController(UserService userService) {
+    public UserController(UserService userService, EmployeeService employeeService) {
         this.userService = userService;
+        this.employeeService = employeeService;
     }
 
     /**
@@ -117,5 +120,105 @@ public class UserController {
         }
         return "redirect:/";
     }
+    @RequestMapping(value = "/profile", method = RequestMethod.GET)
+    public String getProfile(Model model, HttpSession session, User user){
+        User sessionUser = (User) session.getAttribute("LoggedInUser");
+        if (sessionUser!=null) {
+            model.addAttribute("username", sessionUser.getUsername().toUpperCase() + " - Overview");
+            model.addAttribute("abbreviation",(employeeService.findBySSN(sessionUser.getSSN()).getFirstName().charAt(0) + "" + employeeService.findBySSN(sessionUser.getSSN()).getLastName().charAt(0)));
+            model.addAttribute("fullName",(employeeService.findBySSN(sessionUser.getSSN()).getFirstName() + " " + employeeService.findBySSN(sessionUser.getSSN()).getLastName()));
+            model.addAttribute("userRole",sessionUser.getAccounttype()); // Used to display the right nav bar
+            model.addAttribute("firstName",employeeService.findBySSN(sessionUser.getSSN()).getFirstName());
+            model.addAttribute("lastName",employeeService.findBySSN(sessionUser.getSSN()).getLastName());
+            model.addAttribute("phoneNumber",employeeService.findBySSN(sessionUser.getSSN()).getPhoneNumber());
+            model.addAttribute("company",employeeService.findBySSN(sessionUser.getSSN()).getCompany());
+            model.addAttribute("jobtitle",employeeService.findBySSN(sessionUser.getSSN()).getJobTitle());
+        }
+        return "profile";
+    }
+    @RequestMapping(value = "/profile", method = RequestMethod.POST)
+    public String upateProfile(Model model, HttpSession session, User user){
+        User sessionUser = (User) session.getAttribute("LoggedInUser");
+        if (sessionUser!=null) {
+            model.addAttribute("username", sessionUser.getUsername().toUpperCase() + " - Overview");
+            model.addAttribute("abbreviation",(employeeService.findBySSN(sessionUser.getSSN()).getFirstName().charAt(0) + "" + employeeService.findBySSN(sessionUser.getSSN()).getLastName().charAt(0)));
+            model.addAttribute("fullName",(employeeService.findBySSN(sessionUser.getSSN()).getFirstName() + " " + employeeService.findBySSN(sessionUser.getSSN()).getLastName()));
+            model.addAttribute("userRole",sessionUser.getAccounttype()); // Used to display the right nav bar
+
+            model.addAttribute("firstName",employeeService.findBySSN(sessionUser.getSSN()).getFirstName());
+            model.addAttribute("lastName",employeeService.findBySSN(sessionUser.getSSN()).getLastName());
+            model.addAttribute("phoneNumber",employeeService.findBySSN(sessionUser.getSSN()).getPhoneNumber());
+            model.addAttribute("company",employeeService.findBySSN(sessionUser.getSSN()).getCompany());
+            model.addAttribute("jobtitle",employeeService.findBySSN(sessionUser.getSSN()).getJobTitle());
+        }
+        return "profile";
+    }
+    @RequestMapping(value = "employees", method = RequestMethod.POST)
+    public String postEmployees(Model model, HttpSession session, User user) {
+        User sessionUser = (User) session.getAttribute("LoggedInUser");
+        if (sessionUser!=null) {
+            model.addAttribute("username", sessionUser.getUsername().toUpperCase() + " - Overview");
+            model.addAttribute("abbreviation",(employeeService.findBySSN(sessionUser.getSSN()).getFirstName().charAt(0) + "" + employeeService.findBySSN(sessionUser.getSSN()).getLastName().charAt(0)));
+            model.addAttribute("fullName",(employeeService.findBySSN(sessionUser.getSSN()).getFirstName() + " " + employeeService.findBySSN(sessionUser.getSSN()).getLastName()));
+            model.addAttribute("userRole",sessionUser.getAccounttype()); // Used to display the right nav bar
+        }
+        return "employees";
+    }
+    @RequestMapping(value = "employees", method = RequestMethod.GET)
+    public String getEmployees(Model model, HttpSession session, User user) {
+        User sessionUser = (User) session.getAttribute("LoggedInUser");
+        if (sessionUser!=null) {
+            model.addAttribute("username", sessionUser.getUsername().toUpperCase() + " - Overview");
+            model.addAttribute("abbreviation",(employeeService.findBySSN(sessionUser.getSSN()).getFirstName().charAt(0) + "" + employeeService.findBySSN(sessionUser.getSSN()).getLastName().charAt(0)));
+            model.addAttribute("fullName",(employeeService.findBySSN(sessionUser.getSSN()).getFirstName() + " " + employeeService.findBySSN(sessionUser.getSSN()).getLastName()));
+            model.addAttribute("userRole",sessionUser.getAccounttype()); // Used to display the right nav bar
+        }
+        return "employees";
+    }
+    @RequestMapping(value = "realtimeinsights", method = RequestMethod.POST)
+    public String postRealTimeInsights(Model model, HttpSession session, User user) {
+        User sessionUser = (User) session.getAttribute("LoggedInUser");
+        if (sessionUser!=null) {
+            model.addAttribute("username", sessionUser.getUsername().toUpperCase() + " - Overview");
+            model.addAttribute("abbreviation",(employeeService.findBySSN(sessionUser.getSSN()).getFirstName().charAt(0) + "" + employeeService.findBySSN(sessionUser.getSSN()).getLastName().charAt(0)));
+            model.addAttribute("fullName",(employeeService.findBySSN(sessionUser.getSSN()).getFirstName() + " " + employeeService.findBySSN(sessionUser.getSSN()).getLastName()));
+            model.addAttribute("userRole",sessionUser.getAccounttype()); // Used to display the right nav bar
+        }
+        return "realtimeinsights";
+    }
+    @RequestMapping(value = "realtimeinsights", method = RequestMethod.GET)
+    public String getRealTimeInsights(Model model, HttpSession session, User user) {
+        User sessionUser = (User) session.getAttribute("LoggedInUser");
+        if (sessionUser!=null) {
+            model.addAttribute("username", sessionUser.getUsername().toUpperCase() + " - Overview");
+            model.addAttribute("abbreviation",(employeeService.findBySSN(sessionUser.getSSN()).getFirstName().charAt(0) + "" + employeeService.findBySSN(sessionUser.getSSN()).getLastName().charAt(0)));
+            model.addAttribute("fullName",(employeeService.findBySSN(sessionUser.getSSN()).getFirstName() + " " + employeeService.findBySSN(sessionUser.getSSN()).getLastName()));
+            model.addAttribute("userRole",sessionUser.getAccounttype()); // Used to display the right nav bar
+        }
+        return "realtimeinsights";
+    }
+    @RequestMapping(value = "reviews", method = RequestMethod.POST)
+    public String postRequest(Model model, HttpSession session, User user) {
+        User sessionUser = (User) session.getAttribute("LoggedInUser");
+        if (sessionUser!=null) {
+            model.addAttribute("username", sessionUser.getUsername().toUpperCase() + " - Overview");
+            model.addAttribute("abbreviation",(employeeService.findBySSN(sessionUser.getSSN()).getFirstName().charAt(0) + "" + employeeService.findBySSN(sessionUser.getSSN()).getLastName().charAt(0)));
+            model.addAttribute("fullName",(employeeService.findBySSN(sessionUser.getSSN()).getFirstName() + " " + employeeService.findBySSN(sessionUser.getSSN()).getLastName()));
+            model.addAttribute("userRole",sessionUser.getAccounttype()); // Used to display the right nav bar
+        }
+        return "reviews";
+    }
+    @RequestMapping(value = "reviews", method = RequestMethod.GET)
+    public String getRequest(Model model, HttpSession session, User user) {
+        User sessionUser = (User) session.getAttribute("LoggedInUser");
+        if (sessionUser!=null) {
+            model.addAttribute("username", sessionUser.getUsername().toUpperCase() + " - Overview");
+            model.addAttribute("abbreviation",(employeeService.findBySSN(sessionUser.getSSN()).getFirstName().charAt(0) + "" + employeeService.findBySSN(sessionUser.getSSN()).getLastName().charAt(0)));
+            model.addAttribute("fullName",(employeeService.findBySSN(sessionUser.getSSN()).getFirstName() + " " + employeeService.findBySSN(sessionUser.getSSN()).getLastName()));
+            model.addAttribute("userRole",sessionUser.getAccounttype()); // Used to display the right nav bar
+        }
+        return "reviews";
+    }
+
 
 }
