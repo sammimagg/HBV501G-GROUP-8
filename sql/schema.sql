b57d8a1d0edb3376c23beb4aafcc05d4f3d763fc
kbg15 <kbg15@hi.is>
2022-11-09 13:41:18 +0000

    DrivingLog

--- sql/schema.sql
+++ sql/schema.sql
@@ -59,7 +59,7 @@ CREATE TABLE IF NOT EXISTS DrivingLog (
     odometer_start INT,
     odometer_end INT,
     distance_driven INT GENERATED ALWAYS AS ( odometer_start - odometer_end) STORED,
-    cp_km FLOAT,
+    cp_km FLOAT
     );
 
 /*INSERT INTO users (username, password) VALUES ('admin', '$2a$11$pgj3.zySyFOvIQEpD7W6Aund1Tw.BFarXxgLJxLbrzIv/4Nteisii');*/
