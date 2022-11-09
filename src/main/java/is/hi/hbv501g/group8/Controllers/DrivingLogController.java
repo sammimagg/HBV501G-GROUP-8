793a45d5bacd722c96b938386aebf8cc972516a8
kbg15 <kbg15@hi.is>
2022-11-09 16:08:30 +0000

    DrivingLog allt að koma

--- src/main/java/is/hi/hbv501g/group8/Controllers/DrivingLogController.java
+++ src/main/java/is/hi/hbv501g/group8/Controllers/DrivingLogController.java
@@ -2,6 +2,7 @@ package is.hi.hbv501g.group8.Controllers;
 
 import is.hi.hbv501g.group8.Persistence.Entities.Driving;
 import is.hi.hbv501g.group8.Persistence.Entities.Transaction;
+import is.hi.hbv501g.group8.Persistence.Entities.User;
 import is.hi.hbv501g.group8.Services.DrivingService;
 import is.hi.hbv501g.group8.Services.EmployeeService;
 import is.hi.hbv501g.group8.Services.TransactionReviewService;
@@ -10,6 +11,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 public class DrivingLogController {
@@ -21,16 +24,17 @@ public class DrivingLogController {
         this.drivingService = drivingService;
     }
 
-    @RequestMapping(value="/driving", method = RequestMethod.GET)
-    public String drivingGET(Driving driving) {
-        return "drivinglogtests";
+    @RequestMapping(value="/drivinglog", method = RequestMethod.GET)
+    public String drivingGET(Driving driving, User user, HttpSession session) {
+        User sessionUser = (User) session.getAttribute("LoggedInUser");
+        return "drivinglog";
     }
 
-    @RequestMapping(value="/driving/new", method = RequestMethod.POST)
+    @RequestMapping(value="/drivinglog/new", method = RequestMethod.POST)
     public String drivingPOST(Driving driving){
 
         drivingService.save(driving);
 
-        return "drivinglogtests";
+        return "drivinglog";
     }
 }
