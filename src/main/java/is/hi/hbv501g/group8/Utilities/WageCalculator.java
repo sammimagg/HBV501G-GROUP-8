8a5a7bb6406135e029ed57fb3b9fcadbc7f8db37
dorijv <hjv6@hi.is>
2022-11-15 14:18:35 +0000

    #15 Skítaútfærsla að skipta upp tímum, virkar ekki ef þú vinnur framyfir miðnæti, þarf laga, þurfum jesús

--- src/main/java/is/hi/hbv501g/group8/Utilities/WageCalculator.java
+++ src/main/java/is/hi/hbv501g/group8/Utilities/WageCalculator.java
@@ -16,10 +16,10 @@ public class WageCalculator {
     public WageCalculator(double baseSalary) {
         defineConversions();
         this.baseSalary = baseSalary;
-        this.daytime = baseSalary*conversion.get("DV");
-        this.afterhours = baseSalary*conversion.get("EV");
-        this.nighttime = baseSalary*conversion.get("NV");
-        this.overtime = baseSalary*conversion.get("YV");
+        this.daytime = (double) Math.round(baseSalary*conversion.get("DV")*100)/100;
+        this.afterhours = (double) Math.round(baseSalary*conversion.get("EV")*100)/100;
+        this.nighttime = (double) Math.round(baseSalary*conversion.get("NV")*100)/100;
+        this.overtime = (double) Math.round(baseSalary*conversion.get("YV")*100)/100;
     }
 
     private void defineConversions() {
@@ -49,7 +49,7 @@ public class WageCalculator {
         return overtime;
     }
 
-    public double estimateWages(LocalTime shift_start, LocalTime shift_end) {
+    public String estimateWages(LocalTime shift_start, LocalTime shift_end) {
         double totalCost = 0.0;
         double hoursYV = 0.0;
         double hoursEV = 0.0;
@@ -58,14 +58,62 @@ public class WageCalculator {
         double totalHours = 0.0;
 
         Duration workedHours = Duration.between(shift_start, shift_end);
-        totalHours = workedHours.toHours();
+        totalHours = workedHours.toMinutes()/60.0;
 
-        // Worked time > 12 = YV
-        // Mismunur milli 24:00 og shift_end
-        // Mismunur milli 16:00 og shift_end
-        // Mismunur milli 08:00 og shift_end
+        // Worked time > 12 = YV taka frá DV
+        // Mismunur milli 24:00 og shift_end NV
+        // Mismunur milli 16:00 og shift_end EV
+        // Mismunur milli 08:00 og shift_end DV
 
+        if(shift_end.isBefore(LocalTime.of(8, 0))){
+            hoursNV += (Math.min(Duration.between(shift_start, shift_end).toMinutes(),
+                    Duration.between(LocalTime.of(0,0), shift_end).toMinutes()))/60.0;
+        }
 
-        return totalHours;
+        if(shift_end.isBefore(LocalTime.of(23,59))){
+            hoursEV += (Math.min(Duration.between(shift_start, shift_end).toMinutes(),
+                    Duration.between(LocalTime.of(16,0), shift_end).toMinutes())/60.0);
+        }
+
+        hoursYV = Math.max(totalHours-12, 0);
+        hoursDV = totalHours - hoursNV - hoursEV - hoursYV;
+
+        //return hoursNV;
+
+        return String.format("DV: %.2f - EV: %.2f - NV: %.2f - YV: %.2f - TTL: %.2f", hoursDV, hoursEV, hoursNV, hoursYV, totalHours);
+    }
+
+    public String splitHours(LocalTime shift_start, LocalTime shift_end){
+        // 00 - 08: 0 - 480
+        // 08 - 16: 480 - 960
+        // 16 - 24: 960 - 1440
+
+        double hDV, hEV, hNV, hYV, totalHours;
+        int mDV = 0, mEV = 0, mNV = 0, mYV;
+        int start = shift_start.getHour() * 60 + shift_start.getMinute();
+        int end = shift_end.getHour() * 60 + shift_end.getMinute();
+
+        int totalMinutes = end-start;
+        totalHours = totalMinutes/60.0;
+        hYV = Math.max(((end-start) / 60.0) - 12, 0);
+
+        int ptr = start;
+        /*
+        *   Fyrir: ptr = start <= end
+        *   Eftir: ptr > end, stöðugildi DV, EV, NV og YV hækka
+         */
+        while(ptr <= end){
+            if(ptr < 480) mNV++;
+            if(ptr >= 480 && ptr < 960) mDV++;
+            if(ptr >= 960 && ptr < 1440) mEV++;
+            ptr++;
+        }
+
+        // Formatting
+        hDV = mDV/60.0;
+        hNV = mNV/60.0;
+        hEV = mEV/60.0;
+
+        return String.format("DV: %.2f - EV: %.2f - NV: %.2f - YV: %.2f - TTL: %.2f", hDV, hEV, hNV, hYV, totalHours);
     }
 }
--- src/test/java/is/hi/hbv501g/group8/Utilities/WageCalculatorTest.java
+++ src/test/java/is/hi/hbv501g/group8/Utilities/WageCalculatorTest.java
@@ -4,20 +4,35 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalTime;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class WageCalculatorTest {
 
+    private WageCalculator wageCalculator;
+
     @BeforeEach
     void setUp() {
-
+        wageCalculator = new WageCalculator(200000);
     }
 
     @AfterEach
     void tearDown() {
+        wageCalculator = null;
     }
 
     @Test
     void estimateWages() {
+        assertEquals(wageCalculator.splitHours(LocalTime.of(5,5), LocalTime.of(18,0)), 5);
+    }
+
+    @Test
+    void testSalaries() {
+        assertEquals(wageCalculator.getBaseSalary(), 200000);
+        assertEquals(wageCalculator.getDaytime(), 1253.37);
+        assertEquals(wageCalculator.getAfterhours(), 1750.00);
+        assertEquals(wageCalculator.getNighttime(), 1875.00);
+        assertEquals(wageCalculator.getOvertime(), 2077.00);
     }
 }
