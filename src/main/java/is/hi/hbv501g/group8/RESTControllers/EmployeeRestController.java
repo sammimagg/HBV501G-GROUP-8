91eb4add59b2ea2a2ebf64c14e23c063a953d667
halldorjens <hjv6@hi.is>
2023-03-23 15:41:28 +0000

    User -> SessionUser

--- src/main/java/is/hi/hbv501g/group8/RESTControllers/EmployeeRestController.java
+++ src/main/java/is/hi/hbv501g/group8/RESTControllers/EmployeeRestController.java
@@ -6,7 +6,6 @@ import is.hi.hbv501g.group8.Services.TransactionService;
 import is.hi.hbv501g.group8.Services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -28,10 +27,9 @@ public class EmployeeRestController {
     }
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
-    public ResponseEntity<Employee> EmployeeGET(@RequestBody SessionUser sessionUser) {
+    public Employee EmployeeGET(@RequestBody SessionUser sessionUser) {
         System.out.println("test");
-        Employee emp = employeeService.findBySSN(sessionUser.getSsn());
-        return ResponseEntity.ok().body(emp);
+        return employeeService.findBySSN(sessionUser.getSsn());
     }
 
     @RequestMapping(value = "/", method = RequestMethod.POST)
