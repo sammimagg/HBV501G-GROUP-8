bb9680ca2ab64b6f429b1e79a70d9af3c2d79a72
halldorjens <hjv6@hi.is>
2023-04-20 18:39:46 +0000

    hata2

--- src/main/java/is/hi/hbv501g/group8/Persistence/Entities/Jesus.java
+++ src/main/java/is/hi/hbv501g/group8/Persistence/Entities/Jesus.java
@@ -3,10 +3,6 @@ package is.hi.hbv501g.group8.Persistence.Entities;
 public class Jesus {
     private String approved;
 
-    public Jesus(String approved) {
-        this.approved = approved;
-    }
-
     public String getApproved() {
         return approved;
     }
